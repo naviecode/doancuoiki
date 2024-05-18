0af0ea32b2af1e54d4defb5492bc9e123b41b027
Son Dev <sonnq@gmail.com>
2024-05-18 17:11:41 +0700

    commit code

---
+++ DoAnCuoiKi/QuanLyManHinh/ChuongTrinhQuanLy.cs
@@ -0,0 +1,217 @@
+﻿using DoAnCuoiKi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoAnCuoiKi.Validation;
+using DoAnCuoiKi.Function;
+
+
+namespace DoAnCuoiKi.QuanLyManHinh
+{
+    public class ChuongTrinhQuanLy
+    {
+        static void MenuDuAn()
+        {
+            Console.WriteLine("**\t   a. Thêm mới dự án                             **");
+            Console.WriteLine("**\t   b. Chỉnh sửa thông tin dự án                  **");
+            Console.WriteLine("**\t   c. Cập nhập trang thái dự án                  **");
+            Console.WriteLine("**\t   d. Xóa dự án                                  **");
+            Console.WriteLine("**\t   e. Hiển thị các dự án đang có                 **");
+            Console.WriteLine("**\t   f. Tìm kiếm thông tin dự án                   **");
+            Console.WriteLine("**\t   g. Sắp xếp dự án                              **");
+        }
+        static void MenuCongViec()
+        {
+            Console.WriteLine("**\t   a. Thêm mới công việc                         **");
+            Console.WriteLine("**\t   b. Chỉnh sửa công việc                        **");
+            Console.WriteLine("**\t   c. Cập nhập trang thái công việc              **");
+            Console.WriteLine("**\t   d. Xóa công việc                              **");
+            Console.WriteLine("**\t   e. Hiển thị công việc đang có                 **");
+            Console.WriteLine("**\t   f. Các loại tìm kiếm                          **");
+            Console.WriteLine("**\t   g. Các cách sắp xếp                           **");
+
+        }
+        static void MenuPhu()
+        {
+            Console.WriteLine("**\t   a. Đọc file dự án                             **");
+            Console.WriteLine("**\t   b. Đọc file công việc                         **");
+            Console.WriteLine("**\t   c. Ghi file dự án                             **");
+            Console.WriteLine("**\t   d. Ghi file công việc                         **");
+            Console.WriteLine("**\t   e. Danh sách các file đang có                 **");
+        }
+
+        public void ChuongTrinhQuanLyDuAn()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            List<PROJECT> lstProject = new List<PROJECT>();
+            List<TASK> lstTask = new List<TASK>();
+
+            ConsoleKeyInfo luaChon;
+            
+            bool thoat = true, isDuAn = false, isCongViec = false, isFile = false;
+            troVe:
+            Console.WriteLine("------------CHƯƠNG TRÌNH QUẢN LÝ DỰ ÁN CÁ NHÂN------------");
+            Console.WriteLine("**************************MENU*****************************");
+            Console.WriteLine("**\t 1. Thao tác trên dự án                          **");
+            if (isDuAn) MenuDuAn();
+            Console.WriteLine("**\t 2. Thao tác trên nhiệm vụ dự án                 **");
+            if (isCongViec) MenuCongViec();
+            Console.WriteLine("**\t 3. Thao tác trên file                           **");
+            if (isFile) MenuPhu();
+            Console.WriteLine("**\t 4. Xóa dữ liệu đang hiển thị trên màn hình      **");
+            Console.WriteLine("**\t 5. Thoát.                                       **");
+            Console.WriteLine("***********************************************************");
+
+            do
+            {
+                if (!isDuAn && !isCongViec && !isFile)
+                {
+                    luaChon = VALIDATION.NhapOption("Nhập tùy chọn: ");
+                }
+                else
+                {
+                    luaChon = VALIDATION.NhapOption("Nhập tùy chọn (Bấm ESC để trở về menu chính hoặc hủy bỏ thao tác): ");
+                    if(luaChon.Key == ConsoleKey.Escape)
+                    {
+                        isDuAn = false;
+                        isFile = false;
+                        isCongViec = false;
+                        Console.Clear();
+                        goto troVe;
+                    }
+                    Console.WriteLine();
+                }
+
+                //Lua chon menu chính
+                if (!isDuAn && !isCongViec && !isFile)
+                {
+                    switch (luaChon.KeyChar)
+                    {
+                        case '1':
+                            isDuAn = true;
+                            Console.Clear();
+                            goto troVe;
+                        case '2':
+                            isCongViec = true;
+                            Console.Clear();
+                            goto troVe;
+                        case '3':
+                            isFile = true;
+                            Console.Clear();
+                            goto troVe;
+                        case '4':
+                            Console.Clear();
+                            goto troVe;
+                        case '5':
+                            System.Environment.Exit(0);
+                            thoat = false;
+                            break;
+                        default:
+                            Console.WriteLine("Không có thao tác này trong menu!");
+                            break;
+                    }
+                }
+
+
+                //Lua chon menu du an
+                if (isDuAn)
+                {
+                    switch (luaChon.KeyChar)
+                    {
+                        case 'a':
+                            HandleProject.NhapLieuDuAn(lstProject, lstTask);
+                            break;
+                        case 'b':
+                            HandleProject.CapNhapDuAn(lstProject);
+                            break;
+                        case 'c':
+                            HandleProject.CapNhapTrangThai(lstProject);
+                            break;
+                        case 'd':
+                            HandleProject.XoaDuAn(lstProject);
+                            break;
+                        case 'e':
+                            HandleProject.HienThiDuLieuDuAn(lstProject);
+                            break;
+                        case 'f':
+                            HandleProject.TimKiemThongTinDuAn(lstProject);
+                            break;
+                        case 'g':
+                            HandleProject.SapXepThongTinDuAn(ref lstProject);
+                            break;
+                        default:
+                            Console.WriteLine("Bạn đang trong màn hình dự án, vui lòng trở về nếu muốn thực hiện thao tác khác!");
+                            break;
+                    }
+                }
+
+                //Lua chon menu cong viec
+                if (isCongViec)
+                {
+                    switch (luaChon.KeyChar)
+                    {
+                        case 'a':
+                            HandleTask.NhapLieuCongViec(lstProject, lstTask);
+                            break;
+                        case 'b':
+                            HandleTask.CapNhapCongViec(lstTask);
+                            break;
+                        case 'c':
+                            HandleTask.CapNhapTrangThai(lstTask);
+                            break;
+                        case 'd':
+                            HandleTask.XoaDuAn(lstTask);
+                            break;
+                        case 'e':
+                            HandleTask.HienThiCongViecTrongDuAn(lstTask);
+                            break;
+                        case 'f':
+                            HandleTask.TimKiemThongTinCongViec(lstTask);
+                            break;
+                        case 'g':
+                            HandleTask.SapXepThongTinDuAn(ref lstTask);
+                            break;
+                        default:
+                            Console.WriteLine("Bạn đang trong màn hình công việc, vui lòng trở về nếu muốn thực hiện thao tác khác!");
+                            break;
+                    }
+                }
+
+                //Lua chon menu xử lý file
+                if (isFile)
+                {
+                    switch (luaChon.KeyChar)
+                    {
+                        case 'a':
+                            HandleFile.DocFileTxtDuAn(lstProject);
+                            break;
+                        case 'b':
+                            break;
+                        case 'c':
+                            HandleFile.DocFileTxtCongViec(lstProject, lstTask);
+                            break;
+                        case 'd':
+                            break;
+                        case 'e':
+                            HandleFile.DanhSachFile();
+                            break;
+                        default:
+                            Console.WriteLine("Bạn đang trong màn hình xử lý file, vui lòng trở về nếu muốn thực hiện thao tác khác!");
+                            break;
+                    }
+                }
+
+
+
+
+            } while (thoat);
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
---
+++ DoAnCuoiKi/Validation/VALIDATION.cs
@@ -0,0 +1,174 @@
+﻿using DoAnCuoiKi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCuoiKi.Validation
+{
+    internal static class VALIDATION
+    {
+        public static double FormatNumber(double number)
+        {
+            return number;
+        }
+        public static int KiemTraDuLieuSo(string text = null, int length = 0)
+        {
+            int result = 0;
+            bool ktr = false;
+        nhaplai:
+            Console.Write("{0}", text);
+            ktr = int.TryParse(Console.ReadLine(), out result);
+            if (!ktr)
+            {
+                Console.WriteLine("Sai kiểu dữ liệu");
+                goto nhaplai;
+            }
+
+            if (length == 0) return result;
+
+            if (result.ToString().Length > length)
+            {
+                Console.WriteLine("Đã vượt số ký tự cho phép là {0} ký tự ", length);
+                goto nhaplai;
+            }
+
+
+            return result;
+        }
+        public static string KiemTraDoDaiNhapLieu(string text = null, bool required = false, int length = 0)
+        {
+            string result;
+            nhaplai:
+            Console.Write("{0}", text);
+            result = Console.ReadLine();
+
+            if (required && string.IsNullOrEmpty(result.Trim()))
+            {
+                Console.WriteLine("Dữ liệu không được để trống");
+                goto nhaplai;
+            }
+
+            if (length == 0) return result;
+
+            if (result.ToString().Length > length)
+            {
+                Console.WriteLine("Đã vượt số ký tự cho phép là {0} ký tự ", length);
+                goto nhaplai;
+            }
+
+
+            return result;
+        }
+
+        public static ConsoleKeyInfo NhapOption(string text)
+        {
+            nhaplai:
+            Console.Write("{0}", text);
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            string charString = Convert.ToString(keyInfo.KeyChar).Trim();
+            if(string.IsNullOrEmpty(charString))
+            {
+                Console.WriteLine("Lựa chọn không được để trống!");
+                goto nhaplai;
+            }    
+            return keyInfo;
+
+        }
+        public static int KiemTraMaDuAn(List<PROJECT> projects)
+        {
+            nhapLai:
+            int ma_du_an = KiemTraDuLieuSo("Nhập mã dự án: ");
+            PROJECT timKiem = projects.Find(x => x.ma_du_an == ma_du_an);
+            if (projects.Contains(timKiem))
+            {
+                Console.WriteLine("Mã dự án đã tồn tại");
+                goto nhapLai;
+            }
+
+            return ma_du_an;
+        }
+        public static DateTime KiemTraDuLieuThoiGian(string text = null)
+        {
+            DateTime reuslt = DateTime.Now;
+            string str;
+            bool checkDate = false;
+            nhaplai:
+            Console.Write("{0}", text);
+            str = Console.ReadLine();
+            string[] arrStr = str.Split("/");
+            if (arrStr.Length == 3)
+            {
+                checkDate = DateTime.TryParse(arrStr[1] + "/" + arrStr[0] + "/" + arrStr[2], out reuslt);
+            }
+
+            if (!checkDate)
+            {
+                Console.WriteLine("Nhập sai dữ liệu ngày!");
+                goto nhaplai;
+            }
+
+            return reuslt;
+        }
+        public static DateTime SoSanhNgay(DateTime fromDate,string text = null, string fromDateStr = null, string toDateStr = null, string operatorStr = null)
+        {
+
+            DateTime reuslt = DateTime.Now;
+            string str;
+            bool checkDate = false;
+            nhaplai:
+            Console.Write("{0}", text);
+            str = Console.ReadLine();
+            string[] arrStr = str.Split("/");
+            if (arrStr.Length == 3)
+            {
+                checkDate = DateTime.TryParse(arrStr[1] + "/" + arrStr[0] + "/" + arrStr[2], out reuslt);
+            }
+
+            if (!checkDate)
+            {
+                Console.WriteLine("Nhập sai dữ liệu ngày!");
+                goto nhaplai;
+            }
+
+            if (reuslt < fromDate)
+            {
+                Console.WriteLine("{0} {1} hơn {2}!", fromDateStr, operatorStr, toDateStr);
+                goto nhaplai;
+            }
+
+            return reuslt;
+        }
+        public static bool isDate(string data)
+        {
+            DateTime reuslt = DateTime.Now;
+            bool checkDate = false;
+            string[] arrStr = data.Split("/");
+            if (arrStr.Length == 3)
+            {
+                checkDate = DateTime.TryParse(arrStr[1] + "/" + arrStr[0] + "/" + arrStr[2], out reuslt);
+            }
+            else
+            {
+                return checkDate;
+            }
+
+            return checkDate;
+        }
+
+        public static bool isNumberDouble(string data)
+        {
+            return double.TryParse(data, out double a);
+        }
+        public static bool isNumberInt(string data)
+        {
+            return int.TryParse(data, out int a); ;
+        }
+        public static DateTime converStringToDateTime(string data)
+        {
+            string[] arrStr = data.Split("/");
+            return DateTime.Parse(arrStr[1] + "/" + arrStr[0] + "/" + arrStr[2]); ;
+        }
+    }
+}
